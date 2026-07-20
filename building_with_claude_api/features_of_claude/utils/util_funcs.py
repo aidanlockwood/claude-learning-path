0dd6de29107f13e3a1c111cf26c983e4ef9e0925
Aidan Lockwood <aidan.lockwood@blupantera.com>
2026-07-21 08:38:16 +1000

    Features of Claude: Extended Thinking

---
+++ building_with_claude_api/features_of_claude/utils/util_funcs.py
@@ -0,0 +1,69 @@
+# Helper functions
+from anthropic.types import Message
+from anthropic import Anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+load_dotenv()
+
+# Magic string to trigger redacted thinking
+thinking_test_str = "ANTHROPIC_MAGIC_STRING_TRIGGER_REDACTED_THINKING_46C9A13E193C177646C7398A98432ECCCE4C1253D5E2D82641AC0E52CC2876CB"
+
+def api_client_setup(model = 'claude-sonnet-4-5'):
+
+    client = Anthropic()
+    return client, model
+
+client, model = api_client_setup()
+
+def add_user_message(messages, message):
+    user_message = {
+        "role": "user",
+        "content": message.content if isinstance(message, Message) else message,
+    }
+    messages.append(user_message)
+
+
+def add_assistant_message(messages, message):
+    assistant_message = {
+        "role": "assistant",
+        "content": message.content if isinstance(message, Message) else message,
+    }
+    messages.append(assistant_message)
+
+
+def chat(
+    messages,
+    system=None,
+    temperature=1.0,
+    stop_sequences=[],
+    tools=None,
+    thinking = False,
+    thinking_budget = 1024
+):
+    params = {
+        "model": model,
+        "max_tokens": 4000,
+        "messages": messages,
+        "temperature": temperature,
+        "stop_sequences": stop_sequences,
+    }
+
+    if thinking:
+        params['thinking'] = { 
+            "type": "enabled", 
+            "budget_tokens": thinking_budget
+        }
+
+    if tools:
+        params["tools"] = tools
+
+    if system:
+        params["system"] = system
+
+    message = client.messages.create(**params)
+    return message
+
+
+def text_from_message(message):
+    return "\n".join([block.text for block in message.content if block.type == "text"])
