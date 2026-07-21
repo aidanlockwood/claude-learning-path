ab81a8238b6388f782269b0d8717d2b9b7722f98
Aidan Lockwood <aidan.lockwood@blupantera.com>
2026-07-21 12:10:32 +1000

    Model Context Protocol: Defining Resources

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -35,7 +35,24 @@ def edit_document(doc_id: str = Field(description = "ID of the document that wil
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 
 # TODO: Write a resource to return all doc id's
+@mcp.resource( 
+    "docs://documents",
+    mime_type = 'application/json'
+)
+def list_docs(): 
+    return list(docs.keys())
+
 # TODO: Write a resource to return the contents of a particular doc
+@mcp.resource( 
+    'docs://documents/{doc_id}',
+    mime_type = 'text/plain'
+)
+def fetch_doc(doc_id): 
+
+    if doc_id not in docs: 
+        raise ValueError(f'Doc ID with id {doc_id} not found')
+    return docs[doc_id]
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
