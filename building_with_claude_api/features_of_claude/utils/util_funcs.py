459444cea11e7ceec75d1dfe81e38dcc4f885df3
Aidan Lockwood <aidan.lockwood@blupantera.com>
2026-07-21 10:57:12 +1000

    Features of Claude: Code Execution and the Files API

--- building_with_claude_api/features_of_claude/utils/util_funcs.py
+++ building_with_claude_api/features_of_claude/utils/util_funcs.py
@@ -3,9 +3,8 @@ from anthropic.types import Message
 from anthropic import Anthropic
 from dotenv import load_dotenv
 from anthropic.types import ToolParam
+from pathlib import Path
 
-
-load_dotenv()
 load_dotenv()
 
 # Magic string to trigger redacted thinking
@@ -194,3 +193,57 @@ get_current_datetime_schema = ToolParam(
         },
     }
 )
+
+def upload(file_path):
+    path = Path(file_path)
+    extension = path.suffix.lower()
+
+    mime_type_map = {
+        ".pdf": "application/pdf",
+        ".txt": "text/plain",
+        ".md": "text/plain",
+        ".py": "text/plain",
+        ".js": "text/plain",
+        ".html": "text/plain",
+        ".css": "text/plain",
+        ".csv": "text/csv",
+        ".json": "application/json",
+        ".xml": "application/xml",
+        ".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        ".xls": "application/vnd.ms-excel",
+        ".jpeg": "image/jpeg",
+        ".jpg": "image/jpeg",
+        ".png": "image/png",
+        ".gif": "image/gif",
+        ".webp": "image/webp",
+    }
+
+    mime_type = mime_type_map.get(extension)
+
+    if not mime_type:
+        raise ValueError(f"Unknown mimetype for extension: {extension}")
+    filename = path.name
+
+    with open(file_path, "rb") as file:
+        return client.beta.files.upload(file=(filename, file, mime_type))
+
+def list_files():
+    return client.beta.files.list()
+
+
+def delete_file(id):
+    return client.beta.files.delete(id)
+
+
+def download_file(id, filename=None):
+    file_content = client.beta.files.download(id)
+
+    if not filename:
+        file_metadata = get_metadata(id)
+        file_content.write_to_file(file_metadata.filename)
+    else:
+        file_content.write_to_file(filename)
+
+
+def get_metadata(id):
+    return client.beta.files.retrieve_metadata(id)
